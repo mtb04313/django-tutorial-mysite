6d6b9ba6c3261b4f8cf46550fe482811bb5e5e23
mtb04313 <123456789+mtb04313@users.noreply.github.com>
2023-09-04 10:10:50 +0800

    Writing your first Django app, part 7 (admin pages)
    https://docs.djangoproject.com/en/4.2/intro/tutorial07/

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,9 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -15,9 +15,15 @@ class Question(models.Model):
     # def was_published_recently(self):
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
         
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
